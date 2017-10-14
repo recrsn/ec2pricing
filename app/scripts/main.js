8cdb344e52f9338707685cd42c57fa3064134545
varunzxzx <varun995862@gmail.com>
2017-10-14 18:00:06 +0530

    Added sort feature

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,11 @@
 $(function () {
   let pricingData = [];
-
+  let sortOrder = false; // false means the sorting will be in Increasing order and true will be vice versa
+  let $sortIcon = $('#sortIcon');
   let $region = $('#region-select');
   let $type = $('#type-select');
   let $size = $('#size-select');
+  let $data = $('#pricing-data');
 
   let loadData = function (data) {
     for (let region of data.config.regions) {
@@ -45,22 +47,72 @@ $(function () {
     return where;
   };
 
+  let changeData = () => {
+    $data.empty();
+    let where = createFilter();
+    let records = filter(pricingData, where);
+    let sortedRecords = sort(records);
+    for (let record of sortedRecords) {
+      $data.append('<tr><td>' + record.region + '</td><td>' + record.type + '</td><td>' + record.size + '</td><td>' + record.price + '</td></tr>');
+    }
+  }
+
   window.callback = function (data) {
     loadData(data);
     let uniqueValues = unique(pricingData, ['region', 'type', 'size']);
     render(uniqueValues);
-
-    let $data = $('#pricing-data');
-    $('select').on('change', function () {
-      $data.empty();
-      let where = createFilter();
-      let records = filter(pricingData, where);
-
-      for (let record of records) {
-        $data.append('<tr><td>' + record.region + '</td><td>' + record.type + '</td><td>' + record.size + '</td><td>' + record.price + '</td></tr>');
-      }
-    });
+    $('select').on('change',changeData);
   };
+
+  $sortIcon.on("click", () => {
+    if(!sortOrder) {
+      $sortIcon.removeClass('glyphicon-triangle-top');
+      $sortIcon.addClass('glyphicon-triangle-bottom');
+      sortOrder = true;
+    } else {
+      $sortIcon.removeClass('glyphicon-triangle-bottom');
+      $sortIcon.addClass('glyphicon-triangle-top');
+      sortOrder = false;
+    }
+    if($data.html()) {
+      changeData();
+    }
+  })
+
+  let sort = records => {
+    let realData = [];
+    let imagData = [];
+    for(let record of records) {
+      if(record.price != "N/A*") {
+        realData.push(record);
+      } else {
+        imagData.push(record);
+      }
+    }
+    if(!sortOrder) {
+      for(let i=0; i<realData.length - 1; i++) {
+        for(let j=0; j<realData.length - i -1; j++) {
+          if(parseFloat(realData[j].price) > parseFloat(realData[j+1].price)) {
+            let tmp = realData[j];
+            realData[j] = realData[j+1];
+            realData[j+1] = tmp;
+          }
+        }
+      }
+    } else {
+      for(let i=0; i<realData.length - 1; i++) {
+        for(let j=0; j<realData.length - i -1; j++) {
+          if(parseFloat(realData[j].price) < parseFloat(realData[j+1].price)) {
+            let tmp = realData[j];
+            realData[j] = realData[j+1];
+            realData[j+1] = tmp;
+          }
+        }
+      }
+    }
+    let data = realData.concat(imagData);
+    return data;
+  }
 
   let unique = function (data, columns) {
     let values = {};
@@ -81,7 +133,6 @@ $(function () {
       for (let col in where) {
         pass = pass && where[col] === value[col];
       }
-
       return pass;
     })
   };
