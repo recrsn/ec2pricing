f6405528e7be925b666dd171a6657d5873aa8643
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2017-10-16 12:39:01 +0530

    Update main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -179,7 +179,7 @@ $(() => {
   const $currency = $('#currency-select');
 
   function loadData(data) {
-  document.getElementById('loading-bar').style.display = 'none';
+    $('#loading-bar').hide();
     data.config.regions.forEach((region) => {
       region.instanceTypes.forEach((instanceType) => {
         instanceType.sizes.forEach((size) => {
